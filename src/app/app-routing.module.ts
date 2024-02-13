1a03f6836c38cff63f8ecbbf3255a950d1ffc3f2
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2024-02-13 16:15:42 +0700

    fix : Fix bug issue at blog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {MainComponent} from './components/main/main.component';
-import {AboutusComponent} from './components/aboutus/aboutus.component';
-import {PracticeAreasComponent} from './components/practice-areas/practice-areas.component';
-import {PracticeSingleComponent} from './components/practice-areas/practice-single/practice-single.component';
-import {AttorneysComponent} from './components/attorneys/attorneys.component';
-import {BlogComponent} from './components/blog/blog.component';
-import {BlogSingleComponent} from './components/blog/blog-single/blog-single.component';
-import {ContactComponent} from './components/contact/contact.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainComponent } from "./components/main/main.component";
+import { AboutusComponent } from "./components/aboutus/aboutus.component";
+import { PracticeAreasComponent } from "./components/practice-areas/practice-areas.component";
+import { PracticeSingleComponent } from "./components/practice-areas/practice-single/practice-single.component";
+import { AttorneysComponent } from "./components/attorneys/attorneys.component";
+import { BlogComponent } from "./components/blog/blog.component";
+import { BlogSingleComponent } from "./components/blog/blog-single/blog-single.component";
+import { ContactComponent } from "./components/contact/contact.component";
 
 const routes: Routes = [
-  {path: '', component: MainComponent},
-  {path: 'about', component: AboutusComponent},
-  {path: 'practice', component: PracticeAreasComponent},
-  {path: 'practice-areas/single', component: PracticeSingleComponent},
-  {path: 'attorneys', component: AttorneysComponent},
-  {path: 'blog', component: BlogComponent},
-  {path: 'blog/single', component: BlogSingleComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: '', component: MainComponent, pathMatch:  'full' },
+  { path: "", component: MainComponent },
+  { path: "about", component: AboutusComponent },
+  { path: "practice", component: PracticeAreasComponent },
+  { path: "practice/single", component: PracticeSingleComponent },
+  { path: "attorneys", component: AttorneysComponent },
+  { path: "blog/single", component: BlogSingleComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "", component: MainComponent, pathMatch: "full" },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
